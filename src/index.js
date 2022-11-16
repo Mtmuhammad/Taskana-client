d60dd80aa03aba1dbe398ffd19a5247ac2f5b5e2
Mtmuhammad <marcellus.muhammad@yahoo.com>
2022-11-15 20:19:58 -0500

    feat: Add auth security
    Added security to hide dev tools in production.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
