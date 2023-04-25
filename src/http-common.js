de1eb1e1fa47efd3086318b30490613015b332aa
Mtmuhammad <marcellus.muhammad@yahoo.com>
2023-04-24 20:22:52 -0400

    fix: URL Config
    Changed the base url for server access.

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://taskana-server.up.railway.app/";
+const BASE_URL = "https://taskana-server.onrender.com";
 
 
 export default axios.create({ baseURL: BASE_URL });
